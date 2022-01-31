a534ce24c6502d0be1744415fd2628258cea04a2
“Marco <msoberanes@madisoncollege.edu>
2022-01-31 14:21:23 -0600

    This script prints my full name and tells what I learned this week.

---
+++ Week2/first-script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+print("My fist name is Marco")
+print("My last name is Soberanes")
+#print("The arguments are:", str(sys.argv))
+#print("Your First Name is :", sys.argv[1])
+#print("Your last Name is:", sys.argv[2]) 
+print("This week I learned about input, math and output:")
+
